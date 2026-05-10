a45db383f2765aa2c0ef74b0580d15597b5a3012
Artoria <desmia96@gmail.com>
2026-05-11 00:11:51 +0500

    Добавил базовые функции игрока: ходить и двигать камеру

---
+++ Source/TEST/Player/MyGameMode.cpp
@@ -0,0 +1,2 @@
+#include "C:\Users\Akane\Documents\Unreal Projects\TEST\Intermediate\Build\Win64\x64\TESTEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+#include "MyGameMode.h"
